7d5f8d7a7a85d22a54432f5bb24cba5911b3843c
Emma Williams <github@em01.co.uk>
2018-07-13 08:57:40 +0700

    Add further endpoints.

--- routes/customers.js
+++ routes/customers.js
@@ -30,6 +30,7 @@ router.get('/', async(req, res) => {
   res.send(customers)
 });
 
+//Creates a new customer
 router.post('/', async (req, res) => {
   const { error } = validateCustomer(req.body)
   if (error) return res.status(400).send(error.details[0].message)
@@ -43,6 +44,42 @@ router.post('/', async (req, res) => {
   customer = await customer.save();
   res.send(customer);
 })
+
+//updates based on ID
+router.put('/:id', async (req, res) => {
+  const { error } = validateCustomer(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const customer = await Customer.findByIdAndUpdate(req.params.id,
+    {
+      name: req.body.name,
+      isGold: req.body.isGold,
+      phone: req.body.phone
+    }, { new: true });
+
+  if (!customer) return res.status(404).send('The customer with the given ID was not found.');
+
+  res.send(customer);
+});
+
+
+//Deletes the customer
+router.delete('/:id', async (req, res) => {
+  const customer = await Customer.findByIdAndRemove(req.params.id);
+
+  if (!customer) return res.status(404).send('The customer with the given ID was not found.');
+
+  res.send(customer);
+});
+
+//Finds Customer by ID
+router.get('/:id', async (req, res) => {
+  const customer = await Customer.findById(req.params.id);
+
+  if (!customer) return res.status(404).send('The customer with the given ID was not found.');
+
+  res.send(customer);
+});
 
 
 function validateCustomer(customer) {
